23b999cbe60dfadaa163ed23258086b73118da88
sabosabo7 <hiropon.yamakawa@icloud.com>
2022-02-19 23:47:00 +0900

    dfs

---
+++ abc239/d.py
@@ -0,0 +1,78 @@
+from sys import exit, stdin, setrecursionlimit
+
+setrecursionlimit(10 ** 9)
+MOD = 998244353
+INF = float("inf")
+import copy
+
+# input = stdin.readline
+
+# from collections import deque, Counter
+# import heapq
+# from bisect import bisect_left
+# import numpy as np
+# from math import gcd
+# from itertools import permutations, combinations, product
+
+A, B, C, D = map(int, input().split())
+
+S = (
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+    103,
+    107,
+    109,
+    113,
+    127,
+    131,
+    137,
+    139,
+    149,
+    151,
+    157,
+    163,
+    167,
+    173,
+    179,
+    181,
+    191,
+    193,
+    197,
+    199,
+)
+
+ans = "Takahashi"
+for i in range(A, B + 1):
+    for j in range(C, D + 1):
+        if i + j in S:
+            ans = "Aoki"
+            break
+    else:
+        ans = "Takahashi"
+        break
+
+print(ans)
